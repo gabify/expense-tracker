1eb190c84102d78fe968cc0a0e908a6ecab4e77f
gabify <ivangabriel.talaoc@g.batstate-u.edu.ph>
2025-07-20 11:44:06 +0800

    Added circular percentage indicator on savings screen

--- lib/Savings/SavingsDashboard.dart
+++ lib/Savings/SavingsDashboard.dart
@@ -1,7 +1,7 @@
 import 'package:expense_tracker/Savings/SavingsList.dart';
 import 'package:expense_tracker/Services/BudgetProvider.dart';
-import 'package:expense_tracker/Services/savings.dart';
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:provider/provider.dart';
 
 class SavingsDashboard extends StatelessWidget {
@@ -10,6 +10,7 @@ class SavingsDashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final data = context.watch<BudgetProvider>().savings;
+    final totalSavings = context.watch<BudgetProvider>().totalSavings;
     return Scaffold(
         backgroundColor: Colors.grey[50],
         appBar: AppBar(
@@ -35,8 +36,48 @@ class SavingsDashboard extends StatelessWidget {
           ),
       ),
       body: Container(
-        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-        child: SavingsList(savings: data),
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 40),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              CircularPercentIndicator(
+                radius: 120,
+                center: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.savings,
+                      size: 40,
+                      color: Colors.teal[400],
+                    ),
+                    SizedBox(height: 10,),
+                    Text(
+                      'P ${totalSavings}',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'of P 1,000,000.00',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.grey[600],
+                      ),
+                    ),
+                  ],
+                ),
+                progressColor: Colors.teal[400],
+                percent: 0.3,
+                animation: true,
+                backgroundWidth: 14,
+                lineWidth: 14,
+              ),
+              SizedBox(height: 40,),
+              SavingsList(savings: data)
+            ],
+          ),
+        ),
       ),
     );
   }
